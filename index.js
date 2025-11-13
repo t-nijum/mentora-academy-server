6d84bb42643afcf938eec2492fc58942a986ba6c
“t-nijum” <tasmithasinnijum@gmail.com>
2025-11-13 14:06:39 +0100

    added environment variable for mongo db user and pass

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
+
+console.log(process.env);
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://mentoraDBUser:bppiDOlddPvObAq4@cluster0.xr2sv5h.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xr2sv5h.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
